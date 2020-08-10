166032ee72ebb0df438c37774ea11fbdf36f129f
Adogbeji Ibori <68833801+adomaster123@users.noreply.github.com>
2020-08-10 14:04:24 -0500

    Updates

---
+++ precache-manifest.70b4bbcd071b4a3e80319aa0afb2f15f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66086af9a62c1d8bb6c7946db8eb773",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "a0dbd3a974a0faf4cf15",
+    "url": "/quote-machine/static/js/2.67cf7b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.67cf7b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964065f2e8cf2b831346",
+    "url": "/quote-machine/static/js/main.3e72fe1b.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  }
+]);
