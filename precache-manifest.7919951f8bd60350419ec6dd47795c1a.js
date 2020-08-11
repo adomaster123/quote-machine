5861beedcfa9b6e7e1c375838b4a5d177a680f55
Adogbeji Ibori <68833801+adomaster123@users.noreply.github.com>
2020-08-11 03:14:00 -0500

    Updates

---
+++ precache-manifest.7919951f8bd60350419ec6dd47795c1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118a1f66d841c4290b5a5a749e2a8450",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "bbcdfcd5a4256bac3d19",
+    "url": "/quote-machine/static/js/2.e2d6d3e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.e2d6d3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fbe30d136415e73b8a5",
+    "url": "/quote-machine/static/js/main.c5530d26.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quote-machine/precache-manifest.42973db48e8795d022b122af4de07871.js"
+  "/quote-machine/precache-manifest.7919951f8bd60350419ec6dd47795c1a.js"
 );
 
 self.addEventListener('message', (event) => {
